a27a930ce49569310eb5819d7a5cdc2312e783aa
xutianhao <xutianhao@channelwill.cn>
2022-07-09 18:19:17 +0800

    改造成willdesk

--- {{cookiecutter.project_name}}/internal/biz/{{cookiecutter.repo_name}}.go
+++ {{cookiecutter.project_name}}/internal/biz/{{cookiecutter.repo_name}}.go
@@ -13,23 +13,23 @@ var (
 	ErrUserNotFound = errors.NotFound(v1.ErrorReason_USER_NOT_FOUND.String(), "user not found")
 )
 
-// {{cookiecutter.service_name}} is a {{cookiecutter.service_name}} model.
+// {{cookiecutter.repo_service_name}} is a {{cookiecutter.repo_service_name}} model.
 type {{cookiecutter.repo_service_name}} struct {
 	Hello string
 }
 
-// {{cookiecutter.service_name}}Repo is a Greater repo.
+// {{cookiecutter.repo_service_name}}Repo is a Greater repo.
 type {{cookiecutter.repo_service_name}}Repo interface {
-	Save(context.Context, *{{cookiecutter.service_name}}) (*{{cookiecutter.service_name}}, error)
-	Update(context.Context, *{{cookiecutter.service_name}}) (*{{cookiecutter.service_name}}, error)
-	FindByID(context.Context, int64) (*{{cookiecutter.service_name}}, error)
-	ListByHello(context.Context, string) ([]*{{cookiecutter.service_name}}, error)
-	ListAll(context.Context) ([]*{{cookiecutter.service_name}}, error)
+	Save(context.Context, *{{cookiecutter.repo_service_name}}) (*{{cookiecutter.repo_service_name}}, error)
+	Update(context.Context, *{{cookiecutter.repo_service_name}}) (*{{cookiecutter.repo_service_name}}, error)
+	FindByID(context.Context, int64) (*{{cookiecutter.repo_service_name}}, error)
+	ListByHello(context.Context, string) ([]*{{cookiecutter.repo_service_name}}, error)
+	ListAll(context.Context) ([]*{{cookiecutter.repo_service_name}}, error)
 }
 
-// {{cookiecutter.service_name}}Usecase is a {{cookiecutter.service_name}} usecase.
+// {{cookiecutter.repo_service_name}}Usecase is a {{cookiecutter.repo_service_name}} usecase.
 type {{cookiecutter.repo_service_name}}Usecase struct {
-	repo {{cookiecutter.service_name}}Repo
+	repo {{cookiecutter.repo_service_name}}Repo
 	log  *log.Helper
 }
 
@@ -38,7 +38,7 @@ func New{{cookiecutter.repo_service_name}}Usecase(repo {{cookiecutter.repo_servi
 	return &{{cookiecutter.repo_service_name}}Usecase{repo: repo, log: log.NewHelper(logger)}
 }
 
-// Create{{cookiecutter.service_name}} creates a {{cookiecutter.service_name}}, and returns the new {{cookiecutter.service_name}}.
+// Create{{cookiecutter.repo_service_name}} creates a {{cookiecutter.repo_service_name}}, and returns the new {{cookiecutter.repo_service_name}}.
 func (uc *{{cookiecutter.repo_service_name}}Usecase) Create{{cookiecutter.repo_service_name}}(ctx context.Context, g *{{cookiecutter.repo_service_name}}) (*{{cookiecutter.repo_service_name}}, error) {
 	uc.log.WithContext(ctx).Infof("Create{{cookiecutter.repo_service_name}}: %v", g.Hello)
 	return uc.repo.Save(ctx, g)
